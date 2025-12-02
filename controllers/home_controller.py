10d79b09111604207a8ac774f0b47de609e19ee7
LeoReiDelas2 <leonardodshow10@gmail.com>
2025-12-01 22:29:39 -0300

    Editar usuário

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -3,18 +3,24 @@ from .base_controller import BaseController
 from services.pessoas_service import PessoasService
 
 class HomeController(BaseController):
-    def __init__(self, app):
-        super().__init__(app)
+    def _init_(self, app):
+        super()._init_(app)
 
         self.setup_routes()
         self.pessoas_service = PessoasService()
 
     def setup_routes(self):
         self.app.route('/', method=['GET', 'POST'], callback=self.index)
+        self.app.route('/home', method=['GET', 'POST'], callback=self.index_home)
 
     def index(self, db):
         if request.method == 'GET':
             return self.render('homeSemLogin', path = "naoLogado", pathStatus = 'I', user=False)
         else:
             return
-   
+    
+    def index_home(self, db):
+        if request.method == 'GET':
+            return self.render('homeComLogin', path = "naoLogado", pathStatus = 'I', user=False)
+        else:
+            return
--- models/pessoas.py
+++ models/pessoas.py
@@ -1,12 +1,12 @@
 class Pessoas:
-    def __init__(self, name, email, cpf, situacao, senha):
+    def _init_(self, name, email, cpf, situacao, senha):
         self.name = name
         self.email = email
         self.cpf = cpf
         self.situacao = situacao
         self.senha = senha
 
-    def __repr__(self):
+    def _repr_(self):
         return f"<Pessoa {self.name} ({self.email})>"
 
     def to_dict(self):
@@ -34,7 +34,7 @@ class PessoasModel:
     def get_by_id(self, db, pessoa_id):
         try:
             cursor = db.cursor(dictionary=True)
-            cursor.execute("SELECT id_pessoa, name, email, cpf, situacao, senha_hash FROM pessoas WHERE id = %s", (pessoa_id,))
+            cursor.execute("SELECT id_pessoa, nome, email, cpf, situacao, senha_hash FROM pessoas WHERE id_pessoa = %s", (pessoa_id,))
             return cursor.fetchone()
         except Exception as e:
             print(f"Erro ao buscar pessoa com ID {pessoa_id}: {e}")
@@ -66,20 +66,20 @@ class PessoasModel:
             return None
 
 
-    def update_pessoa(self, db, pessoa: Pessoas):
+    def update_pessoa(self, db, pessoa, id_pessoa):
         try:
             cursor = db.cursor()
             sql = """
                 UPDATE pessoas
-                SET name = %s, email = %s, cpf = %s, situacao = %s, senha_hash = %s
+                SET nome = %s, email = %s, cpf = %s
                 WHERE id_pessoa = %s
             """
-            cursor.execute(sql, (pessoa.name, pessoa.email, pessoa.cpf, pessoa.situacao, pessoa.senha, pessoa.id))
+            cursor.execute(sql, (pessoa['nome'], pessoa['email'], pessoa['cpf'], id_pessoa))
             db.commit()
             return True
         except Exception as e:
             db.rollback()
-            print(f"Erro ao atualizar pessoa ID {pessoa.id}: {e}")
+            print(f"Erro ao atualizar pessoa ID {id_pessoa}: {e}")
             return False
 
 
